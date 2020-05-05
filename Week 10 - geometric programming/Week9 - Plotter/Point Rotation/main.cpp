d38037fb00844c173ea15a7c54980dd9df19fec8
michelle <michelle.zhang56@gmail.com>
2020-05-05 18:50:35 +1200

    new code

---
+++ Week 10 - geometric programming/Week9 - Plotter/Point Rotation/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+
+//Written by Michelle Zhang BSE20021
+//Create a command console prompt tool that rotates a Cartesian coordinate
+//about another Cartesian coordinate by a number of degrees.
+
+// Point Rotation
+int main() {
+
+}
