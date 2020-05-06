41563af0c71de54ceeceaa480f94b7e2891abf55
Michelle <michelle.zhang56@gmail.com>
2020-05-06 19:02:05 +1200

    wip circle

--- Week 10 - geometric programming/Week9 - Plotter/Week9 - Plotter/main.cpp
+++ Week 10 - geometric programming/Week9 - Plotter/Week9 - Plotter/main.cpp
@@ -17,66 +17,7 @@ void getCircumference(int, int);
 int main() {
 	
 	
-	//take in user input
-	//cout << "Input centre coordinates " << endl;
-	//cout << "x coordinate: ";
-	//cin >> x;
-	//cout << "y coordinate: ";
-	//cin >> y;
-	//cout << endl;
-	//cout << "Input horizontal radius and vertical radius " << endl;
-	//cout << "h. radius: " ;
-	//cin >> h_radius;
-	//cout << "v. radius: ";
-	//cin >> v_radius;
-	//cout << endl;
-	//cout << "Input number of angles to plot: ";
-	//cin >> n_angles;
 
-	/*angle = 360 / n_angles;
-	getCircumference(h_radius, v_radius);*/
-
-	float r;
-
-	cout << " Enter the Radius" << endl;
-
-	cin >> r;
-
-	float pr = 2; // pr is the aspected pixel ratio which is almost equal to 2
-
-	for (int i = -r; i <= r; i++) // loop for horizontal movement
-
-	{
-
-		for (int j = -r; j <= r; j++) // loop for vertical movement
-
-		{
-
-			float d = ((i * pr) / r) * ((i * pr) / r) + (j / r) * (j / r); //multiplying the i variable with pr to equalize pixel-width with the height
-
-			if (d > 0.95 && d < 1.08) // approximation
-
-			{
-
-				cout << "*";
-
-			}
-
-			else
-
-			{
-
-				cout << " ";
-
-			}
-
-		}
-
-		cout << endl;
-
-	}
-
-	return 0;
 	
 
 	
